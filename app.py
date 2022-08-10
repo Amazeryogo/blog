ac4030251f16fee627a66b8f75b7cffb48c29015
Suvid Datta <suvid.datta@gmail.com>
2022-08-10 23:30:40 +0530

    IDK PART ONE

--- app.py
+++ app.py
@@ -4,8 +4,20 @@ import waitress
 
 app = Flask(__name__)
 
+TITLE = "Suvid's Blog"
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title=TITLE)
 
-waitress.run(app, host='0.0.0.0',port=5000)
+@app.route('/about')
+def about():
+    return render_template('about.html', title=TITLE)
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title=TITLE)
+
+
+
+waitress.serve(app, host='0.0.0.0',port=5000)
